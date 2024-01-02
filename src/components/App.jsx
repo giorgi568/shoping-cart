6d5b514747fa64a59f11b11f563b4d5f04e20059
giorgi568 <mateshvili02@mail.ru>
2024-01-02 19:25:36 +0400

    added button and navigation for card page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import '../App.css';
 import { Outlet, useLocation, useNavigate, Link } from 'react-router-dom';
 import styles from '../styles/App.module.css';
+import { createContext } from 'react';
+
+export const CartContext = createContext([]);
 
 function App() {
   const location = useLocation().pathname;
   let navigate = useNavigate();
-  const sliderStyle =
-    location === '/store' ? { transform: 'translateX(100%)', borderRadius:' 0px 8px 8px 0px' } : {};
+  let sliderStyle = {};
+  if (location === '/cart') {
+    sliderStyle = { display: 'none' };
+  } else if (location === '/store') {
+    sliderStyle = {
+      transform: 'translateX(100%)',
+      borderRadius: ' 0px 8px 8px 0px',
+    };
+  }
   return (
     <>
       <div className={styles.nav}>
@@ -18,6 +28,11 @@ function App() {
             Store
           </button>
           <div className={styles.slider} style={sliderStyle}></div>
+        </div>
+        <div className={styles.cartLogoWrapper}>
+          <Link to={'/cart'}>
+            <img src='./shopping_cart.svg' alt='shopping cart logo' />
+          </Link>
         </div>
       </div>
       <Outlet />
