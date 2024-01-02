e4ccb9ea9d2cda7b3c44d3a00fb022e9fc71e01d
giorgi568 <mateshvili02@mail.ru>
2024-01-03 02:50:15 +0400

    added checkout logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,6 +6,19 @@ import styles from '../styles/Cart.module.css';
 export default function Cart() {
   const { cartItems } = useContext(CartContext);
   console.log(cartItems, 55555);
+  const getTotalPrice = () => {
+    let total = 0;
+    if (cartItems.length > 0) {
+      cartItems.forEach((item) => {
+        if (item.qnt) {
+          total += item.price * item.qnt;
+        } else {
+          total += item.price;
+        }
+      });
+    }
+    return total;
+  };
 
   return (
     <div>
@@ -15,6 +28,19 @@ export default function Cart() {
             cartItems.map((item, index) => {
               return <CartItemCard item={item} index={index} key={item.id} />;
             })}
+        </div>
+        <div className={styles.checkoutBtn}>
+          {cartItems.length > 0 && (
+            <button
+              className={styles.btn}
+              onClick={(e) => {
+                e.preventDefault;
+                window.alert('Checkout not working (not a real store)');
+              }}
+            >
+              Chekout {getTotalPrice()}$
+            </button>
+          )}
         </div>
       </div>
     </div>
